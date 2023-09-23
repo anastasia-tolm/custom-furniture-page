bc192001c0fca7d1a9f6e4fb96afe0ae539ef2e2
anastasia-tolm <118930033+anastasia-tolm@users.noreply.github.com>
2023-09-23 11:14:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ if (iconMenu) {
         menuBody.classList.toggle('menu__body_active');
     });
 }
+
+// Прокрутка при клике 
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight; //- document.querySelector('header').offsetHeight; для фикисированной шапки
+
+            if (iconMenu.classList.contains('menu__icon_active')) {
+                document.body.classList.remove('body_lock'); // Запрет скролла при открытом меню
+                iconMenu.classList.remove('menu__icon_active');
+                menuBody.classList.remove('menu__body_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
