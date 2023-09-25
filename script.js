61ef055a4cf1c5f8058320eaadb3cb3500abfca2
anastasia-tolm <118930033+anastasia-tolm@users.noreply.github.com>
2023-09-25 10:55:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if (footerYearElement) {
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
-if (iconMenu) {
+if (iconMenu && menuBody) {
     iconMenu.addEventListener("click", function (e) {
         document.body.classList.toggle('body_lock'); // Запрет скролла при открытом меню
         iconMenu.classList.toggle('menu__icon_active');
